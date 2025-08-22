062435b9e9fea35e4b222d34e4e8454a0e0478e3
gtopinio <mctopinio@up.edu.ph>
2025-08-22 11:40:06 +0800

    chore: setup husky with conventional commits and lint-staged

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // New feature
+        'fix', // Bug fix
+        'docs', // Documentation
+        'style', // Formatting, missing semicolons, etc
+        'refactor', // Code change that neither fixes a bug nor adds a feature
+        'test', // Adding missing tests
+        'chore', // Maintain, build, CI, dependencies
+        'perf', // Performance improvements
+        'build', // Changes to build system
+        'ci', // Changes to CI configuration
+        'revert', // Revert a previous commit
+      ],
+    ],
+    'subject-case': [2, 'never', ['upper-case']],
+    'subject-max-length': [2, 'always', 72],
+    'header-max-length': [2, 'always', 100],
+  },
+};
